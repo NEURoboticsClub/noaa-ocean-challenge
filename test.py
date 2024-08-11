291466f6a7bcf8e06e99c888480d4d593a48986e
PandaHood <niksanderson0@gmail.com>
2024-08-11 16:56:36 -0400

    summary

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import os
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="sterfesh.yaml", epochs=3)  # train the model
+model.train(data="sterfesh.yaml",epochs=3)  # train the model
 #metrics = model.val()  # evaluate model performance on the validation set
 
 results = model.predict(source="seafloor_footage.mp4",save=True)
