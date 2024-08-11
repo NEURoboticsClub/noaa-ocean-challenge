15ef8aa33da3ffb66618b867abf406c5a28d9207
PandaHood <niksanderson0@gmail.com>
2024-08-11 14:41:09 -0400

    Summary

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
+import json
+import os
 
 # Load a model
-#model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco8.yaml", epochs=3)  # train the model
+model.train(data="sterfesh.yaml", epochs=3)  # train the model
 #metrics = model.val()  # evaluate model performance on the validation set
 
 results = model.predict(source="seafloor_footage.mp4",save=True)
