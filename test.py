53486f8039f0eadb7dad14698eb7feaad258e45c
PandaHood <niksanderson0@gmail.com>
2024-08-23 17:39:05 -0400

    Summary

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-from ultralytics import YOLO
-import json
-import os
+import torch
 
-# Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
-# Use the model
-model.train(data="sterfesh.yaml",imgsz=(1920,1080),epochs=3)  # train the model
-#metrics = model.val()  # evaluate model performance on the validation set
-
-results = model.predict(source="seafloor_footage.mp4",imgsz=(1920,1080),save=True) 
+print(torch.__version__)
